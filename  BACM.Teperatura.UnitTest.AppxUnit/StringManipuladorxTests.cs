a38668d83b1b6a3dff2c45dbac071d846ef4facf
Bryan19K <126676499+Bryan19K@users.noreply.github.com>
2023-10-22 15:55:42 -0600

    Agregar archivos de proyecto.

---
+++  BACM.Teperatura.UnitTest.AppxUnit/StringManipuladorxTests.cs
@@ -0,0 +1,40 @@
+﻿using BACM.StringManipulador;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BACM.StringManipulador.UnitTest.AppxUnit
+{
+    public class StringManipulador
+    {
+        [Fact]
+        public void ReverseString_ShouldReverseInput()
+        {
+            // Arrange
+            StringManipulator manipulator = new StringManipulator();
+            string input = "Hello";
+
+            // Act
+            string result = manipulator.ReverseString(input);
+
+            // Assert
+            Assert.Equal("olleH", result);
+        }
+
+        [Fact]
+        public void RemoveSpaces_ShouldRemoveSpaces()
+        {
+            // Arrange
+            StringManipulator manipulator = new StringManipulator();
+            string input = "Hello World";
+
+            // Act
+            string result = manipulator.RemoveSpaces(input);
+
+            // Assert
+            Assert.Equal("HelloWorld", result);
+        }
+    }
+}
